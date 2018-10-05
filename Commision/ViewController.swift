43ce0994980df4186356add2de05474290a4c793
Grace Paulette <gpaulette0292@jh-mba158-15.d214.org>
2018-10-05 11:14:44 -0500

    Initial Commit

---
+++ Commision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Commision
+//
+//  Created by Grace Paulette on 10/5/18.
+//  Copyright © 2018 John Hersey HIgh School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
