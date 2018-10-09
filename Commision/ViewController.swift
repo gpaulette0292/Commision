2313177ea73a400e8c3ae9179f300ba2ac655b77
Grace Paulette <gpaulette0292@jh-mba158-15.d214.org>
2018-10-09 11:07:20 -0500

    added IBOutlets and IBActions

--- Commision/ViewController.swift
+++ Commision/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var totalPayLabel: UILabel!
+    @IBOutlet weak var commissionPayTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction func calculateOnTapped(_ sender: UIButton) {
+    }
+    
 }
 
